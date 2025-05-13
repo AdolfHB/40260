b99dcaf64d2239dbafd05b4a331447cb5cfbc0c9
Yo <Yo@FEI.uv>
2025-05-12 18:44:48 -0600

    07-Formulario

--- html00/formulario.js
+++ html00/formulario.js
@@ -1,12 +1,26 @@
 console.log("Inicio")
 
+/* function mostrar(){
+    console.log("Parametros: ")
+    let user = document.getElementById("user")
+    let password = document.getElementById("password")
+    console.log(user.value)
+    console.log(password.value)
+} */
+
+const mostrar = () => {
+    console.log("Parametros: ")
+    let user = document.getElementById("user")
+    let password = document.getElementById("password")
+    console.log(user.value)
+    console.log(password.value)
+
+    let formulario = document.getElementsByTagName("form")
+    console.log("Formulario")
+    user.setAttribute("name", "q")
+    password.setAttribute("password", "password")
+    formulario[0].setAttribute("action", "https://google.com/search")
+    formulario[0].submit()
+}
 const boton = document.getElementById("enviar")
-boton.addEventListener("click", 
-    () => {
-        console.log("Paramtros")
-        let user = document.getElementById("user")
-        let password = document.getElementById("password")
-        console.log(user.value)
-        console.log(password.value)
-    }
-)
+boton.addEventListener("click", mostrar)
